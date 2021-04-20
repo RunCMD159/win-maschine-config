db469e96b207992b4f6f4a0eb4c9003ca496f078
fschmuck <florian.schmuck@bearingpoint.com>
2021-04-20 11:38:29 +0200

    [SETUP] - Install software for home maschine

--- chocolatey-setup/prepare_workstation.bat
+++ chocolatey-setup/prepare_workstation.bat
@@ -7,23 +7,14 @@
 
 :: Browsers
 choco install firefox -y
-choco install firefox-dev --pre  -y
 choco install googlechrome -y
 
 :: Editors
 choco install vscode -y
-choco install intellijidea-ultimate -y
 
 :: Dev tools
 choco install git -y
-choco install maven -y
-choco install gradle -y
-choco install nodejs-lts -y
 choco install correttojdk -y
-choco install corretto8jdk -y
-choco install putty -y
-choco install docker-desktop -y
-choco install postman -y
 
 
 :::: Media
@@ -32,7 +23,6 @@ choco install vlc -y
 :::: Utilities + other
 choco install 7zip.install -y
 choco install keepass -y
-choco install winscp -y
 choco install spotify -y
 choco install whatsapp -y
 choco install signal -y
